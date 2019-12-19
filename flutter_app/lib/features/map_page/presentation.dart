a266a3524ad9da6c25722b69b8464bcc6adcad08
Johan Reitan <johan.reitan@knowit.no>
2019-12-19 17:35:20 +0100

    display "people" in the map

--- flutter_app/lib/features/map_page/presentation.dart
+++ flutter_app/lib/features/map_page/presentation.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -6,11 +7,36 @@ class MapPage extends StatelessWidget {
     target: LatLng(58.9177924, 5.7020611),
     zoom: 12,
   );
+  static const _tripId = 'y0YhKJG9lMAVbgECzSaN';
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: _initialPosition,
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collection('trips')
+            .document(_tripId)
+            .collection('people')
+            .snapshots()
+            .handleError(() {
+          Scaffold.of(context).showSnackBar(
+              SnackBar(content: Text('Failed to find any people 🦗️')));
+        }),
+        builder: (context, snapshot) {
+          return GoogleMap(
+            initialCameraPosition: _initialPosition,
+            markers: snapshot.data.documents.map(
+              (person) {
+                return Marker(
+                  markerId: MarkerId(person.documentID),
+                  position: LatLng(
+                    person.data['currentLocation']['latitude'],
+                    person.data['currentLocation']['longitude'],
+                  ),
+                  infoWindow: InfoWindow(title: person.data['name']),
+                );
+              },
+            ).toSet(),
+          );
+        });
   }
 }
