2a21f2af457cf85a7782a1e73b2e439935388af8
Johan Reitan <johan.reitan@gmail.com>
2020-02-25 13:59:27 +0100

    replace Google Maps with flutter_map and Mapbox tiles, and add avatar widget map markers

--- flutter_app/lib/features/map_page/presentation.dart
+++ flutter_app/lib/features/map_page/presentation.dart
@@ -1,18 +1,19 @@
+import 'package:background_location/background_location.dart';
+import 'package:bratur/features/map_page/person_map_marker.dart';
 import 'package:bratur/models/user.dart';
+import 'package:bratur/secrets.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:user_location/user_location.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   final bool isSharingLocation;
   final Function onShareLocationTapped;
+  final String tripId;
   final List<User> users;
   final GlobalKey<ScaffoldState> scaffoldKey;
-
-  final _initialPosition = CameraPosition(
-    target: LatLng(58.9177924, 5.7020611),
-    zoom: 12,
-  );
-  final String tripId;
 
   MapPage({
     @required this.isSharingLocation,
@@ -23,34 +24,84 @@ class MapPage extends StatelessWidget {
   });
 
   @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final _mapController = MapController();
+
+  @override
   Widget build(BuildContext context) {
+    final markers = widget.users.map((user) {
+      return Marker(
+        point: LatLng(
+          user.currentLocation.latitude,
+          user.currentLocation.longitude,
+        ),
+        anchorPos: AnchorPos.align(AnchorAlign.center),
+        builder: (ctx) => PersonMapMarker(
+          key: Key(user.userId),
+          user: user,
+        ),
+      );
+    }).toList();
+
     return Scaffold(
-      key: scaffoldKey,
-      body: GoogleMap(
-        initialCameraPosition: _initialPosition,
-        myLocationButtonEnabled: false,
-        myLocationEnabled: true,
-        markers: users.map(
-          (user) {
-            return Marker(
-              markerId: MarkerId(user.userId),
-              position: LatLng(
-                user.currentLocation.latitude,
-                user.currentLocation.longitude,
+      key: widget.scaffoldKey,
+      body: FutureBuilder(
+        future: BackgroundLocation.checkPermissions(),
+        initialData: PermissionStatus.unknown,
+        builder: (context, AsyncSnapshot<PermissionStatus> permissionsStatus) {
+          return FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              center: LatLng(58.9177924, 5.7020611),
+              zoom: 12,
+              plugins: [
+                UserLocationPlugin(),
+              ],
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate: "https://api.tiles.mapbox.com/v4/"
+                    "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+                additionalOptions: {
+                  'accessToken': mapboxAccessToken,
+                  'id': 'mapbox.streets',
+                },
               ),
-              infoWindow: InfoWindow(title: user.name),
-            );
-          },
-        )?.toSet(),
+              MarkerLayerOptions(markers: markers),
+              if (permissionsStatus.data.value ==
+                  PermissionStatus.granted.value)
+                UserLocationOptions(
+                  context: context,
+                  mapController: _mapController,
+                  markers: markers,
+                  updateMapLocationOnPositionChange: false,
+                  zoomToCurrentLocationOnLoad: false,
+                  moveToCurrentLocationFloatingActionButton:
+                      FloatingActionButton(
+                    child: Icon(Icons.my_location),
+                    onPressed: null,
+                  ),
+                  fabHeight: 48,
+                  fabWidth: 48,
+                  fabBottom: 16,
+                  fabRight: 16,
+                ),
+            ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => onShareLocationTapped(),
+        onPressed: () => widget.onShareLocationTapped(),
         icon: Icon(Icons.track_changes),
-        label: Text(isSharingLocation ? 'Stop sharing' : 'Share my location'),
-        backgroundColor: isSharingLocation
+        label: Text(
+            widget.isSharingLocation ? 'Stop sharing' : 'Share my location'),
+        backgroundColor: widget.isSharingLocation
             ? Theme.of(context).buttonColor
             : Theme.of(context).accentColor,
-        foregroundColor: isSharingLocation
+        foregroundColor: widget.isSharingLocation
             ? Theme.of(context).textTheme.button.color
             : Theme.of(context).accentTextTheme.button.color,
       ),
