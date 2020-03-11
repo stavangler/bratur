f8cc938cd0e7c546fd48557cf323d8815e3f9b61
Johan Reitan <johan.reitan@gmail.com>
2020-03-11 21:32:53 +0100

    show speakers and details

--- web_app/src/features/trip-details/agenda/model.ts
+++ web_app/src/features/trip-details/agenda/model.ts
@@ -2,8 +2,9 @@ import firebase from "../../../firebase-config"
 import { dataModel, CollectionDataModel } from "../../../store/data-model"
 
 export interface Speaker {
-  id: number
-  email: string
+  userId: string
+  name: string
+  photoUrl: string
 }
 
 export interface AgendaItem {
@@ -11,9 +12,11 @@ export interface AgendaItem {
   title: string
   description: string
   location: string
-  speakers: Speaker[]
+  speakers: firebase.firestore.DocumentReference<Speaker>[]
   topics: string[]
   track: string
+  startTime: Date
+  duration: number
 }
 
 export interface AgendaModel extends CollectionDataModel<AgendaItem, string> {}
@@ -28,8 +31,21 @@ const agenda: AgendaModel = {
         description: snapshot.data().description,
         location: snapshot.data().location,
         track: snapshot.data().track,
-        speakers: snapshot.data().speakers,
+        speakers: (snapshot.data()
+          .speakers as firebase.firestore.DocumentReference[]).map(ref =>
+          ref.withConverter<Speaker>({
+            fromFirestore: snapshot => ({
+              userId: snapshot.id,
+              name: snapshot.data().name,
+              photoUrl: snapshot.data().photoUrl,
+            }),
+            toFirestore: modelObject => modelObject,
+          })
+        ),
         topics: snapshot.data().topics,
+        startTime: (snapshot.data()
+          .startTime as firebase.firestore.Timestamp).toDate(),
+        duration: snapshot.data().duration,
       }),
       toFirestore: (modelObject: AgendaItem) => modelObject,
     }
--- web_app/src/features/trip-details/agenda/trip-agenda.tsx
+++ web_app/src/features/trip-details/agenda/trip-agenda.tsx
@@ -1,16 +1,9 @@
 import React, { useEffect } from "react"
 import { useStoreActions, useStoreState } from "../../../store/hooks"
-import {
-  Container,
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  Grid,
-  Typography,
-} from "@material-ui/core"
+import { Container, Grid, Typography } from "@material-ui/core"
 import { useParams } from "react-router-dom"
 import { makeStyles } from "@material-ui/core/styles"
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
-import LocationOnIcon from "@material-ui/icons/LocationOn"
+import { AgendaEvent } from "./agenda-event"
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -19,15 +12,6 @@ const useStyles = makeStyles(theme => ({
   },
   expansionPanels: {
     width: "100%",
-  },
-  dot: {
-    height: 12,
-    width: 12,
-    borderRadius: "50%",
-    display: "inline-block",
-  },
-  monoFont: {
-    fontFamily: "Roboto Mono, monospace",
   },
 }))
 
@@ -52,66 +36,11 @@ export default function TripAgenda() {
         </Grid>
 
         <Grid item className={classes.expansionPanels}>
-          {data.map(i => (
-            <ExpansionPanel
-              key={i.id}
-              TransitionProps={{ unmountOnExit: true }}
-            >
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <Grid container spacing={1}>
-                  <Grid item xs={12}>
-                    <Typography variant="body1">{i.title}</Typography>
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Grid container direction="row">
-                      <Grid item>
-                        <LocationOnIcon fontSize="small" color="disabled" />
-                      </Grid>
-                      <Grid item>
-                        <Typography variant="body2">{i.location}</Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Grid container direction="row" spacing={1}>
-                      {[i.track, ...i.topics].map(topic => (
-                        <Grid item>
-                          <span
-                            className={classes.dot}
-                            style={{ backgroundColor: generateColor(topic) }}
-                          />
-                          &nbsp;
-                          <Typography
-                            variant="caption"
-                            className={classes.monoFont}
-                          >
-                            {topic}
-                          </Typography>
-                        </Grid>
-                      ))}
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </ExpansionPanelSummary>
-            </ExpansionPanel>
+          {data.map(item => (
+            <AgendaEvent key={item.id} item={item} />
           ))}
         </Grid>
       </Grid>
     </Container>
   )
-}
-
-function generateColor(seed: string): string {
-  let hash = 0
-  for (let i = 0; i < seed.length; i++) {
-    hash = seed.charCodeAt(i) + ((hash << 5) - hash)
-    hash = hash & hash
-  }
-  const rgb = [0, 0, 0]
-  for (let i = 0; i < 3; i++) {
-    rgb[i] = (hash >> (i * 8)) & 255
-  }
-  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
 }
