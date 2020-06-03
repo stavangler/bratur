45180d42b5d57fb1d510c7409bce6d1eb1f49dca
Frode Anonsen <frode@anonsen.org>
2020-06-03 11:07:31 +0200

    start new trip

--- web_app/src/features/trips/create-trip.tsx
+++ web_app/src/features/trips/create-trip.tsx
@@ -1,31 +1,71 @@
-import React, { FormEvent, useState } from 'react'
+import React, {FormEvent, useState} from 'react'
+import {Container, ExpansionPanel, ExpansionPanelSummary, Grid, Typography} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import {makeStyles} from "@material-ui/core/styles";
 
-function CreateTrip () {
+const useStyles = makeStyles(theme => ({
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  expansionPanels: {
+    width: "100%",
+  },
+  dot: {
+    height: 12,
+    width: 12,
+    borderRadius: "50%",
+    display: "inline-block",
+  },
+  monoFont: {
+    fontFamily: "Roboto Mono, monospace",
+  },
+}))
+
+function CreateTrip() {
+  const classes = useStyles()
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
 
-  function handleSubmit (e: FormEvent<HTMLFormElement>) {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
     console.log(title, description)
   }
 
   return (
-    <div className="container">
-      <form className="white" onSubmit={handleSubmit}>
-        <h5 className="grey-text text-darken-3">Create a New Trip</h5>
-        <div className="input-field">
-          <input type="text" id='title' onChange={(e) => setTitle(e.target.value)} />
-          <label htmlFor="title">Destination</label>
-        </div>
-        <div className="input-field">
-          <textarea id="content" className="materialize-textarea" onChange={(e) => setDescription(e.target.value)} />
-          <label htmlFor="content">Description</label>
-        </div>
-        <div className="input-field">
-          <button className="btn pink lighten-1">Create</button>
-        </div>
-      </form>
-    </div>
+    <Container maxWidth="lg" className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h3">New trip</Typography>
+        </Grid>
+
+        <Grid item className={classes.expansionPanels}>
+          <ExpansionPanel TransitionProps={{unmountOnExit: true}}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+              <Grid container spacing={1}>
+
+                <Grid item xs={12}>
+                  <Typography variant="body1">title</Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Grid container direction="row">
+                    <Grid item>
+                      <LocationOnIcon fontSize="small" color="disabled"/>
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="body2">location</Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+              </Grid>
+            </ExpansionPanelSummary>
+          </ExpansionPanel>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
